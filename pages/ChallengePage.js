e7d0457467f6454e5ac6f70f8c84125267b8cab4
Rachuri Guna Susmitha <rachuriguna@gmail.com>
2025-05-25 23:53:39 +0530

    added back heading validation!

--- pages/ChallengePage.js
+++ pages/ChallengePage.js
@@ -1,7 +1,7 @@
 export class ChallengePage {
     constructor(page) {
         this.page = page;
-        this.heading = page.locator('#challenge-list-title');
+        this.heading = page.getByRole('heading', { name: 'Hackathons, Programming Challenges, and Coding Competitions' });
         this.liveSection = page.locator("//div[@class='ongoing challenge-list']");
         this.challengeCards = this.liveSection.locator("//div[@class='challenge-card-modern']");
     }
@@ -13,18 +13,9 @@ export class ChallengePage {
 
     async isHeadingVisible() {
         console.log('Checking if heading is visible...');
-        try {
-            await this.heading.waitFor({ state: 'visible', timeout: 15000 });
-            const visible = await this.heading.isVisible();
-            return visible;
-        } catch (e) {
-            // Take a screenshot and save the HTML for debugging
-            await this.page.screenshot({ path: 'ci-debug.png', fullPage: true });
-            const html = await this.page.content();
-            require('fs').writeFileSync('ci-debug.html', html);
-            console.error('Heading not visible:', e);
-            return false;
-        }
+        await this.heading.waitFor({ state: 'visible' });
+        const visible = await this.heading.isVisible();
+        return visible;
     }
 
     async getLiveChallengeLinksAndTitles() {
