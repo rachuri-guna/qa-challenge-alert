cecb0a32ba4f0b8da96a2e3ceff49517cc3dbd61
Rachuri Guna Susmitha <84988457+rachuri-guna@users.noreply.github.com>
2025-05-25 14:41:57 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -33,6 +33,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
+    outputDir: 'test-results/',
   },
 
   /* Configure projects for major browsers */
