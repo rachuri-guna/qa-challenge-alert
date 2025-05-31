cc2912c7e4f5a9484e304f2acc91f9ebd74a2684
Rachuri Guna Susmitha <84988457+rachuri-guna@users.noreply.github.com>
2025-05-31 23:03:37 +0530

    forbidOnly

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ export default defineConfig({
     video: 'retain-on-failure',
     outputDir: 'test-results/',
   },
-  forbidOnly: true,
+  forbidOnly: false,
 
   /* Configure projects for major browsers */
   projects: [
